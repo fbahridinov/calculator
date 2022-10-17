16cef38e34e0b736e54826159a298845958ed48b
firuz <gaberud@yandex.ru>
2022-10-17 20:56:49 +0300

    version 0.3

--- user_interface.py
+++ user_interface.py
@@ -1,9 +1,11 @@
 # Мозг
 import model_addition as ma
+import model_division as md
+import view
 
 
 def select_menu(a):
     if a == 1:
-        number_a = int(input('Число А: '))
-        number_b = int(input('Число Б: '))
-        ma.fn_addition(number_a, number_b)
+        ma.fn_addition(view.get_numbers_a(), view.get_numbers_b())
+    elif a == 2:
+        md.fn_division(view.get_numbers_a(), view.get_numbers_b())
--- view.py
+++ view.py
@@ -19,4 +19,19 @@ def show_menu():
     return menu
 
 
+def get_numbers_a():
+    number_a = input('Число А: ')
+    if number_a.find('.') == True:
+        number_a = float(number_a)
+    else:
+        number_a = int(number_a)
+    return number_a
 
+
+def get_numbers_b():
+    number_b = input('Число Б: ')
+    if number_b.find('.') == True:
+        number_b = float(number_b)
+    else:
+        number_b = int(number_b)
+    return number_b
